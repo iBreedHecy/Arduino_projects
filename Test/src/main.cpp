9fd313a2e5936c8ac19001ab0fca8c3a8e52340d
Mackelly <65777489+iBreedHecy@users.noreply.github.com>
2020-11-19 15:25:06 +0200

    test

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -1,12 +1,38 @@
 #include <Arduino.h>
 
+volatile bool enc_state = false;
+volatile int napr = 4;
+int test;
+volatile unsigned long last_enc_rot;
+
+void encoderFunc() {
+  napr = digitalRead(4);
+  if(millis() - last_enc_rot > 50 && napr != 3){
+    enc_state = true;
+
+    
+    last_enc_rot = millis();
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN,OUTPUT);
+  Serial.begin(9600);
+  pinMode(3,INPUT_PULLUP);
+  pinMode(4,INPUT_PULLUP);
+  attachInterrupt(1, encoderFunc,FALLING);
+
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN,! digitalRead(LED_BUILTIN));
-  //delay(500);
-  // put your main code here, to run repeatedly:
+  if(enc_state == true){
+
+    Serial.print("значение : ");
+    if(napr == 0)test--;else test++;
+    Serial.println(test);
+    napr = 3;
+    enc_state = false;
+
+  }
 }
+
