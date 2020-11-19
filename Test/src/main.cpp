85f1ea73573445d52296479b36c73c61645b4041
Mackelly <65777489+iBreedHecy@users.noreply.github.com>
2020-11-19 14:00:08 +0200

    test

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -2,8 +2,11 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED_BUILTIN,OUTPUT);
 }
 
 void loop() {
+  digitalWrite(LED_BUILTIN,! digitalRead(LED_BUILTIN));
+  delay(500);
   // put your main code here, to run repeatedly:
 }
