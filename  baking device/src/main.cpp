8f62ee4768b54de92ff8b62781977f9f96893ca5
iBreedHecy <65777489+iBreedHecy@users.noreply.github.com>
2020-11-22 15:48:20 +0300

    test

---  baking device/src/main.cpp
+++  baking device/src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // Устанавливаем дисплей
+
+int8_t varCurrent = 20;
+int8_t varExposition;
 
 void setup() {
-  // put your setup code here, to run once:
+  lcd.init();                     
+  lcd.backlight();// Включаем подсветку дисплея
+  lcd.print(" Backing device");
+  delay(500);
+  lcd.setCursor(0, 1);
+  lcd.print("  by MACKELLY");
+  delay(1000);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("EXP:        A:");
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Устанавливаем курсор на вторую строку и нулевой символ.
+  
+  lcd.setCursor(4, 0);  
+  // Выводим на экран количество секунд с момента запуска ардуины
+  lcd.print(millis()/1000); 
+  lcd.print("sec"); 
+  lcd.setCursor(14, 0);  
+  // Выводим на экран количество секунд с момента запуска ардуины
+  lcd.print(varCurrent); 
+}
+
+void StartBeking() {
+
 }
